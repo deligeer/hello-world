37d046c83d8d45bf0f2e9a819d5508bd3047bb55
deligeer <geerdeli@aliyun.com>
2020-03-21 10:39:13 -0400

    Test files for test-repo-789 fork

---
+++ install.sh
@@ -0,0 +1,13 @@
+
+apt-get install git -y
+apt-get install curl -y
+apt-get install locate -y
+apt-get install telnetd -y
+apt-get install proftpd -y
+git clone https://github.com/edwardoughton/pysim5g.git
+
+mkdir conda
+cd conda/
+
+# refer to conda/install.sh
+
